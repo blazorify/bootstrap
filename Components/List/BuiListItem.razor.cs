be11dec80b00c4da12a6232a9b2f87995afbb698
Roman Popovici <lambidu@live.com>
2024-10-04 19:43:30 +0300

    wip (list): allow list items to have href attribute

--- Components/List/BuiListItem.razor.cs
+++ Components/List/BuiListItem.razor.cs
@@ -1,4 +1,9 @@
+using System;
+using Microsoft.AspNetCore.Components;
+
 namespace Blazorify.Bootstrap {
 	public partial class BuiListItem : BuiContentComponentBase {
+		[Parameter]
+		public String? Href { get; set; }
 	}
 }
