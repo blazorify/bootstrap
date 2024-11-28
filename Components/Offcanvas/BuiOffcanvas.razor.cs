48a4ed0ab4394a901e54d7d6125efdaf5fa6980b
Roman Popovici <lambidu@live.com>
2024-11-28 11:25:13 +0200

    refactor (offcanvas): replaced child components with properties

--- Components/Offcanvas/BuiOffcanvas.razor.cs
+++ Components/Offcanvas/BuiOffcanvas.razor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using Blazorify.Bootstrap.Attributes;
 using Microsoft.AspNetCore.Components;
 
@@ -9,8 +10,37 @@ namespace Blazorify.Bootstrap {
 		public Boolean Open { get; set; } = false;
 
 		[Parameter]
+		public Boolean Closeable { get; set; } = true;
+
+		[Parameter]
 		[BindClass("offcanvas-start", Placement.Start)]
 		[BindClass("offcanvas-end", Placement.End)]
 		public Placement Placement { get; set; } = Placement.Start;
+
+		[Parameter]
+		public RenderFragment? Header { get; set; }
+
+		[Parameter]
+		public RenderFragment? Body { get; set; }
+
+		[Parameter]
+		public EventCallback OnShow { get; set; }
+
+		[Parameter]
+		public EventCallback OnHide { get; set; }
+
+		public async Task Show() {
+			this.Open = true;
+
+			await this.OnShow.InvokeAsync();
+			await this.InvokeAsync(this.StateHasChanged);
+		}
+
+		public async Task Hide() {
+			this.Open = false;
+
+			await this.OnHide.InvokeAsync();
+			await this.InvokeAsync(this.StateHasChanged);
+		}
 	}
 }
