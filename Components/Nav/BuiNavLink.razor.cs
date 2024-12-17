7eb1681272a4747db8a308c8909ba0ac6effeb64
Roman Popovici <lambidu@live.com>
2024-12-17 13:36:58 +0200

    wip: use NavigationManager to detect if link is active

--- Components/Nav/BuiNavLink.razor.cs
+++ Components/Nav/BuiNavLink.razor.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Threading.Tasks;
 using Blazorify.Bootstrap.Attributes;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 namespace Blazorify.Bootstrap {
 	public partial class BuiNavLink : BuiContentComponentBase {
+		[Inject]
+		private NavigationManager? navigationManager { get; set; }
+
 		[Parameter]
 		[BindClass("active", true)]
 		public Boolean Active { get; set; } = false;
@@ -18,5 +22,22 @@ namespace Blazorify.Bootstrap {
 
 		[Parameter]
 		public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+		protected override async Task OnParametersSetAsync() {
+			ArgumentNullException.ThrowIfNull(this.navigationManager);
+
+			await base.OnParametersSetAsync();
+
+			if (!String.IsNullOrEmpty(this.Href)) {
+				var currentPath = this.navigationManager.ToAbsoluteUri(this.navigationManager.Uri).AbsolutePath;
+				var targetPath = this.navigationManager.ToAbsoluteUri(this.Href).AbsolutePath;
+
+				if (currentPath.Equals(targetPath, StringComparison.OrdinalIgnoreCase)) {
+					this.Active = true;
+				}
+
+				await this.InvokeAsync(StateHasChanged);
+			}
+		}
 	}
 }
--- Components/Typography/BuiLink.razor.cs
+++ Components/Typography/BuiLink.razor.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 
 namespace Blazorify.Bootstrap {
 	public partial class BuiLink : BuiButton {
+		[Inject]
+		private NavigationManager? navigationManager { get; set; }
+
 		[Parameter]
 		public String? Href { get; set; }
+
+		protected override async Task OnParametersSetAsync() {
+			ArgumentNullException.ThrowIfNull(this.navigationManager);
+
+			await base.OnParametersSetAsync();
+
+			if (!String.IsNullOrEmpty(this.Href)) {
+				var currentPath = this.navigationManager.ToAbsoluteUri(this.navigationManager.Uri).AbsolutePath;
+				var targetPath = this.navigationManager.ToAbsoluteUri(this.Href).AbsolutePath;
+
+				if (currentPath.Equals(targetPath, StringComparison.OrdinalIgnoreCase)) {
+					this.Active = true;
+				}
+
+				await this.InvokeAsync(StateHasChanged);
+			}
+		}
 	}
 }
