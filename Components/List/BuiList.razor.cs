ef44a7bd7e60d285c08cfa80f3f8cd8615df7d3e
Roman Popovici <lambidu@live.com>
2025-01-12 20:56:41 +0200

    add (list): added flush and numbered parameters

--- Components/List/BuiList.razor.cs
+++ Components/List/BuiList.razor.cs
@@ -1,4 +1,20 @@
+using System;
+using System.Threading.Tasks;
+using Blazorify.Bootstrap.Attributes;
+using Microsoft.AspNetCore.Components;
+
 namespace Blazorify.Bootstrap {
 	public partial class BuiList : BuiItemsComponentBase {
+		[Parameter]
+		[BindClass("list-group-flush", true)]
+		public Boolean Flush { get; set; } = false;
+
+		[Parameter]
+		[BindClass("list-group-numbered", true)]
+		public Boolean Numbered { get; set; } = false;
+
+		protected override async Task OnBindClassAppliedAsync() {
+			await base.OnBindClassAppliedAsync();
+		}
 	}
 }
